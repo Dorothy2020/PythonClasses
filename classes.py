98ee8b098fa39535344580fa876b41eb61b897db
Dorothy_Akoth <dorothy@untapped-global.com>
2023-03-21 10:30:58 +0300

    feat: pythonClass

---
+++ classes.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__ (self,age,weight,height):
+        self.age=age
+        self.weight=weight
+        self.height=height
+
+
+    def walk(self):
+            print("walk")
+            user=Person(30,20,50)
+
+            user.walk()
+
+    
